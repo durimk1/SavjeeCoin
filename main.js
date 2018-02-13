8594519e65cc82da9a5d43dda0228965f3d8cab1
John Murphy <murphy.john1991@gmail.com>
2018-02-13 14:37:50 +0000

    Updated to version from video 3 incorporating mining rewards.

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const SHA256 = require("crypto-js/sha256");
 
+class Transaction{
+    //a transaction always comes from someone, goes to someone and has a certain amount of coins
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
 class Block {
-  constructor(index, timestamp, data, previousHash = '') {
-    this.index = index;
+  constructor(timestamp, transactions, previousHash = '') {
     this.previousHash = previousHash;
     this.timestamp = timestamp;
-    this.data = data;
+    this.transactions = transactions;
     this.hash = this.calculateHash();
     //the nonce is required so that there is a value that can be changed as if none of the contents of the block changed then 
     //the hash would never change! This is essential to avoid the mining while loop going on forever
@@ -15,7 +23,7 @@ class Block {
 
   calculateHash() {
       //remember to take into account all of the properties of the block apart from the hash (including the nonce)
-      return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) +this.nonce).toString();
+      return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.transactions) +this.nonce).toString();
   }
 
   mineBlock(difficulty){
@@ -40,20 +48,68 @@ class Blockchain{
     constructor() {
         this.chain = [this.createGenesisBlock()];
         this.difficulty = 2;
+        //create an empty array that will store pending transactions (transactions between block creation intervals)
+        this.pendingTransactions = []; 
+        //reward for mining a block.
+        this.miningReward = 100;
     }
 
     createGenesisBlock() {
-        return new Block(0, "01/01/2017", "Genesis block", "0");
+        return new Block(Date.parse("01/01/2017"), [], "0");
     }
 
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty); //mines the new block 
-        this.chain.push(newBlock);
+    /* this method is deleted and replaced by the minePendingTransactions() method*/
+    // addBlock(newBlock) {
+    //     newBlock.previousHash = this.getLatestBlock().hash;
+    //     newBlock.mineBlock(this.difficulty); //mines the new block 
+    //     this.chain.push(newBlock);
+    // }
+
+    //accepts a wallet address of the miner as a param so that coins can be sent to that address
+    minePendingTransactions(miningRewardAddress){
+        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
+        block.mineBlock(this.difficulty);
+
+        console.log('Block successfully mined!');
+        this.chain.push(block);
+
+        //you need this method to reset the pending transactions as they would be no longer pending.
+        //but it also includes the transaction to send the reward to the miner in the new array of pending txs
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress, this.miningReward)
+        ];
+    }
+
+    //a method to add new transactions to the pendingTransactions array.
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+
+    //like bitcoin, there is no "balance" per se, it needs to be calculated by examining the transactions history for the address
+    getBalanceofAddress(address){
+        //just define a balance and set it to 0 initially
+        let balance = 0;
+
+        //loop over all of the blocks in the blockchain
+        for(const block of this.chain){
+            //because each block contains multiple transactions we loop over each transaction in the block
+            for(const trans of block.transactions){
+                //if you are from address, you sent so balance must be reduced.
+                if(trans.fromAddress === address){
+                    balance -= trans.amount;
+                }
+                //if you are to address, you receive so balance must be increased.
+                if(trans.toAddress === address){
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance; 
     }
 
     isChainValid(){ //a series of checks to validate the blocks.
@@ -80,8 +136,16 @@ class Blockchain{
 
 let savjeeCoin = new Blockchain();
 
-console.log('Mining block 1...');
-savjeeCoin.addBlock(new Block(1, "20/07/2017", { amount: 4 }));
+//the 'address1' and 'address2' are just for demo, these would be public keys of a wallet address in a real version.
+savjeeCoin.createTransaction(new Transaction('address1', 'address2', 100));
+savjeeCoin.createTransaction(new Transaction('address1', 'address2', 50));
 
-console.log('Mining block 2...');
-savjeeCoin.addBlock(new Block(2, "20/07/2017", { amount: 8 }));
+console.log('\n Starting the miner.');
+savjeeCoin.minePendingTransactions('xaviers-address');
+
+console.log('\nBalance of xavier is', savjeeCoin.getBalanceofAddress('xaviers-address'));
+
+console.log('\n Starting the miner.');
+savjeeCoin.minePendingTransactions('xaviers-address');
+
+console.log('\nBalance of xavier is', savjeeCoin.getBalanceofAddress('xaviers-address'));
